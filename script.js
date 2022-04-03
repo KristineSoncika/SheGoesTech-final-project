e076e12bbb41f2493c4a33dda8a59065b9c0799d
kristine <57680335+kristtiine@users.noreply.github.com>
2022-04-03 13:39:55 +0300

    added active state to nav bar

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(() => {
-  // scroll to top button
+  // === scroll to top button
   $(window).scroll(function () {
     if (
+      // button appears when number of pixels from the top of body or html (documentElement) when scrolled is > 500
       document.body.scrollTop > 500 ||
       document.documentElement.scrollTop > 500
     ) {
@@ -12,5 +13,36 @@ $(() => {
   });
   $(".to-top").on("click", () => {
     $(window).scrollTop(0);
+  });
+
+  //  === adds active state to current page in navigation bar
+
+  //   1. gets current page URL
+  let url = window.location.href;
+  //   2. removes # from url if any
+  url = url.substring(
+    0,
+    url.indexOf("#") === -1 ? url.length : url.indexOf("#")
+  );
+  //   3. removes parameters from URL if any
+  url = url.substring(
+    0,
+    url.indexOf("?") == -1 ? url.length : url.indexOf("?")
+  );
+  //   4. selects file name
+  url = url.substring(url.lastIndexOf("/") + 1);
+  //   5. if file name is not available, set it to index.html
+  if (url === "") {
+    url = "index.html";
+  }
+  //   6. loop through nav-item links
+  $(".nav-item a").each(function () {
+    //   select href
+    let href = $(this).attr("href");
+    // check filename
+    if (url === href) {
+      // add active class
+      $(this).addClass("active-state");
+    }
   });
 });
