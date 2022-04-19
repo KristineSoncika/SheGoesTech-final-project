b1c808aaa5ba949c5d68729af9546824e0c55d27
Eva Nolendorfa <eva@Evas-MacBook-Air.local>
2022-04-19 13:43:28 +0300

    last changes

---
+++ HPScript.js
@@ -0,0 +1,49 @@
+///////////////////////////////////////
+//ADD COMENTS at LOCAL STORAGE
+let itemList = document.getElementById('items'); 
+let form = document.getElementById('addForm');
+console.log(form);
+
+
+
+// FOrm submit event
+if(form){
+  form.addEventListener('submit',addItem); // tikko tiek nospiests submint, tā tiek izsaukta funkcija addItem
+}
+
+let coments = JSON.parse(localStorage.getItem('coments')); // kā string saglabājas komentāri
+if (coments) {
+  coments.forEach(function(coment){  // ja ir storage komentāri (kuri salikti array), tad katru komentāru pievieno html
+    addComent(coment);
+  });
+} else {  
+  coments = []; // kad nekas nebūs komentāros, uztaisās tukšs array, lai ir kaut kas kur likt iekšā
+}
+
+//add item funkcija
+function addItem(e){ 
+    e.preventDefault(); // šo vajag paredzēt pie submit/apstirināt
+   
+    // get input value
+    let newItem = document.getElementById('item').value;
+
+    addComent(newItem);  // šeit pievieno html
+    addToStorage(newItem); //šajā brīdī kaut kas array jau ir (vai nu tukšums vai komentārs)
+}
+function addToStorage(coment){
+  coments.push(coment); //ieliku iepriekš izveidotajā array 47 vai 53 rindiņa, liek array, lai varētu pārtaisīt par string un ieliekt locastorage
+  localStorage.setItem('coments',JSON.stringify(coments)); //ieliek komentāru locastorage array ar komentāriem
+}
+
+function addComent(coment){
+  //create new li element
+  let li = document.createElement('li');
+  //add class
+  li.className = 'list-group-item';
+  // add text node with input value
+  li.appendChild(document.createTextNode(coment)); //text node is variable newItem, ko ievada lietotājs
+
+
+  itemList.appendChild(li);
+}
+
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/////////////////////////////////
 // BOOKS SEARCH //
 
 let filter = document.getElementById('filter');
@@ -20,9 +21,8 @@ function filterItems(e){
 
     //convert HTML collection to an array
     Array.from(items).forEach(function(item){
-        console.log(item);
         let itemName = item.children[0].textContent;
-        if(itemName.toLowerCase().indexOf(text) != -1){
+        if(itemName.toLowerCase().indexOf(text) != -1){ // search pēc children 0, kas ir paragrāfs autoru nemeklē!!!!!!!!!!
             item.style.display = 'block';
         } else {
             item.style.display = 'none';
@@ -31,94 +31,19 @@ function filterItems(e){
 }
 
 
-//ADD COMENTS
-let form = document.getElementById('addForm');
-console.log(form);
-
-
-
-// FOrm submit event
-if(form){
-  form.addEventListener('submit',addItem); // tikko tiek nospiests submint, tā tiek izsaukta funkcija addItem
-}
-
-let coments = JSON.parse(localStorage.getItem('coments')); // kā string saglabājas komentāri
-if (coments) {
-  coments.forEach(function(coment){  // ja ir storage komentāri (kuri salikti array), tad katru komentāru pievieno html
-    addComent(coment);
-  });
-} else {  
-  coments = []; // kad nekas nebūs komentāros, uztaisās tukšs array, lai ir kaut kas kur likt iekšā
-}
-
-//add item funkcija
-function addItem(e){ 
-    e.preventDefault(); // šo vajag paredzēt pie submit/apstirināt
-    console.log('notika');
-    // get input value
-    let newItem = document.getElementById('item').value;
-
-    addComent(newItem);  // šeit pievieno html
-    addToStorage(newItem); //šajā brīdī kaut kas array jau ir (vai nu tukšums vai komentārs)
-}
-function addToStorage(coment){
-  coments.push(coment); //ieliku iepriekš izveidotajā array 47 vai 53 rindiņa, liek array, lai varētu pārtaisīt par string un ieliekt locastorage
-  localStorage.setItem('coments',JSON.stringify(coments)); //ieliek komentāru locastorage array ar komentāriem
-}
-
-function addComent(coment){
-  //create new li element
-  let li = document.createElement('li');
-  //add class
-  li.className = 'list-group-item';
-  // add text node with input value
-  li.appendChild(document.createTextNode(coment)); //text node is variable newItem, ko ievada lietotājs
-
-  //create the delite button element
-  let deleteBtn = document.createElement('button');
-
-  //add clases to delite button
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete'; 
-  
-  //appent text node
-  deleteBtn.appendChild(document.createTextNode('X'));
-
-  //now that we have all that we have to append the button into li
-  li.appendChild(deleteBtn);
-
-  itemList.appendChild(li);
-}
-
-//remove item nospiežot x
-if(itemList){
-  itemList.addEventListener('click', removeItem);
-}
-
-
-function removeItem(e){
-    if(e.target.classList.contains('delete')){ //jo bez šī, ķer visus klikšķus, aks ir iekšā li ielikti, tāpēc vajag if statemnt, lai dabūtu tieši uz tās pogas
-        if(confirm('Are you sure?')){
-            let li = e.target.parentElement;
-            itemList.removeChild(li); // li is a child of a itemList
-        }
-    }
-}
-
-
-
-
 //contacts//
 //alert for Send button
 const btnSend = document.getElementById('send');
+
 if(btnSend) {
   btnSend.addEventListener('click', showMessage);
 }
-
-
 function showMessage(){
     alert('We Got Your message!\nWe Will answer in 2 to 5 days!\nThank You!');
   };
 
+
+  
 $(() => {
   // === Scroll to top button ===
   $(window).scroll(function () {
